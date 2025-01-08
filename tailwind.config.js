fdf23134957105acc396e51e549b709d48ba968f
tuannguyen1704 <teemoanh2817@gmail.com>
2025-01-09 03:15:53 +0700

    them file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import { Flowbite } from "flowbite-react/tailwind";
+import flowbite from "flowbite-react/tailwind";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
